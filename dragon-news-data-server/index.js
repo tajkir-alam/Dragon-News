d2a457ec26a6de37b4906eb49b121f878fe70ab4
tajkir-alam <tajkir.alam@gmail.com>
2023-04-30 10:53:20 +0600

    server/API done

--- dragon-news-data-server/index.js
+++ dragon-news-data-server/index.js
@@ -13,10 +13,25 @@ app.get('/news', (req, res) => {
     res.send(news);
 })
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const singleNews = news.find(n => n._id === id);
+    res.send(singleNews);
+})
+
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
-app.listen(port , () => {
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        return res.send(news);
+    }
+    const category = news.filter(n => parseInt(n.category_id) === id);
+    res.send(category);
+})
+
+app.listen(port, () => {
     console.log(`Dragon News API is running  on  ${port}`);
 })
